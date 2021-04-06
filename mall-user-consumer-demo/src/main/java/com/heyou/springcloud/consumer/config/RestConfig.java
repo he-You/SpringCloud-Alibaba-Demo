6a01e0ff1c8597ce77f62361fb8241f6051bd33f
heyou-macbook <heyou_0423@163.com>
2021-04-06 23:45:54 +0800

    add:Rest配置类

---
+++ mall-user-consumer-demo/src/main/java/com/heyou/springcloud/consumer/config/RestConfig.java
@@ -0,0 +1,28 @@
+package com.heyou.springcloud.consumer.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.client.loadbalancer.LoadBalancerClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
+
+/**
+ * @author heyou(heyou_0423 @ 163.com)
+ * @date 2021/4/6 下午10:51
+ */
+@Configuration
+public class RestConfig {
+    @Autowired
+    private LoadBalancerClient loadBalanced;
+
+    @Bean
+    @LoadBalanced
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
+//        RestTemplate restTemplate = new RestTemplate();
+//        restTemplate.setInterceptors(Collections.singletonList(new LoadBalancerInterceptor(loadBalancer)));
+//        return restTemplate;
+    }
+
+}
