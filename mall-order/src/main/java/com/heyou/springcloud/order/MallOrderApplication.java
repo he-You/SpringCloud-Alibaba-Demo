31520a3975792725fa776621ff8871626f1a9e15
heyou-macbook <heyou_0423@163.com>
2021-04-09 00:22:26 +0800

    add:订单服务启动类

---
+++ mall-order/src/main/java/com/heyou/springcloud/order/MallOrderApplication.java
@@ -0,0 +1,17 @@
+package com.heyou.springcloud.order;
+
+import com.alibaba.druid.spring.boot.autoconfigure.DruidDataSourceAutoConfigure;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+
+/**
+ * @author heyou(heyou_0423 @ 163.com)
+ * @date 2021/4/8 下午11:04
+ */
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class, DruidDataSourceAutoConfigure.class})
+public class MallOrderApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(MallOrderApplication.class,args);
+    }
+}
