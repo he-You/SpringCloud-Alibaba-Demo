aa21037f4e8fe097c5fd6255571f9c0de03418ea
heyou-macbook <heyou_0423@163.com>
2021-04-09 00:23:36 +0800

    update:修改注入的 bean变量名称

--- mall-user-consumer-demo/src/main/java/com/heyou/springcloud/consumer/config/RestConfig.java
+++ mall-user-consumer-demo/src/main/java/com/heyou/springcloud/consumer/config/RestConfig.java
@@ -3,6 +3,7 @@ package com.heyou.springcloud.consumer.config;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.client.loadbalancer.LoadBalancerClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalancerInterceptor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
@@ -14,7 +15,7 @@ import org.springframework.web.client.RestTemplate;
 @Configuration
 public class RestConfig {
     @Autowired
-    private LoadBalancerClient loadBalanced;
+    private LoadBalancerClient loadBalancer;
 
     @Bean
     @LoadBalanced
